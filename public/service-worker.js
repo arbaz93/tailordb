c2cbc4c9ec16e6efcca831c6dd1985bea8c75bf1
arbaz93 <arbaz.k480@yahoo.com>
2026-02-27 01:59:24 +0500

    updated service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,6 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js");
 
 workbox.routing.registerRoute(
-  ({request}) => requestAnimationFrame.destination === "image",
+  ({request}) => request.destination === "image",
   new workbox.strategies.CacheFirst()
 );
